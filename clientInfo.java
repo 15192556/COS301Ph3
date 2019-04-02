cba430f2636cea4b36e14037a4f06ce0e829589a
DuncanSmallwood <duncansmallwood@yahoo.com>
2019-04-02 22:17:31 +0200

    partially 'unpseudofied' the pseudocode in clientInfo.getEmail, also made .addClient and .hashFunction static in order to allow them to be used in .getEmail

--- clientInfo.java
+++ clientInfo.java
@@ -1,11 +1,17 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 public class clientInfo {
 
     private static String CLIENT_HASH_TABLE = "clients.txt";
-    
+
     // Get email from Client Information System API. Parameters to be submitted are ClientID
     public static String getEmail (String clientID) {
-        /* Psedocode 
+        /* Pseudocode
             if (clientID is in the client hash table ("clients.txt"))
                 if (timestamp from this entry in the hash table is not older than 10 minutes)
                     return client email as it exists in the table
@@ -13,23 +19,67 @@ public class clientInfo {
                     submit GET request of client email to Client Information System API
                     update hash table entry timestamp and email address
                     return address
-            
+
             else
                 submit GET request of client email to Client Information System API
                 add new entry to hash table and override the previous entry if there is a collision
                 return address
         */
-    
+        int clientIDHash = clientInfo.hashFunction(clientID);
+        String hashClientID = /*clients.txt(clientIDHash)*/;
+        LocalDateTime currTime = LocalDateTime.now();
+        if (hashClientID == clientID)
+        {
+            if (currTime.minusMinutes(10) < /*clients.txt(clientIDHash).time*/)
+            {
+                return /*clients.txt(clientIDHash).email*/;
+            }
+        }
+
+        try
+        {
+            URL apiMethod = new URL("https://cos301-ocean-cis-api.herokuapp.com/email");
+            HttpURLConnection apiCall = (HttpURLConnection) apiMethod.openConnection();
+            apiCall.setRequestMethod("POST");
+            String JSONBody = "{\"system\":\"NS\",\"client_id\":\"" + clientID + "\"}";
+            byte[] out = JSONBody.getBytes(StandardCharsets.UTF_8);
+            int bodyLength = out.length;
+
+            apiCall.setFixedLengthStreamingMode(bodyLength);
+            apiCall.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            apiCall.connect();
+            apiCall.getOutputStream().write(out);
+            apiCall.getInputStream();//the response
+            /*
+            not sure on the format of the response ect. so not sure how to split it up into "clientID" and "email"
+            add new entry to hash table and override the previous entry if there is a collision
+            return address
+            * */
+
+        }
+        catch(MalformedURLException e)
+        {/*if we are here, the apiMethod variable has been initialised wrong*/}
+        catch(IOException e)
+        {/*
+            thrown by:
+            apiMethod.openConnection()
+            apiCall.setRequestMethod() [presumably, PotocolExeption inherits from IOException
+            apiCall.connect()
+            apiCall.getOutPutStream()
+            apiCall.getInputStream()
+
+        */}
+
         return "u16050607@tuks.co.za";
     }
-    
+
     // Adds client to the hash table. The hash table will be stored in a text file and has a maximum of 10 entries, and records the clientID, email address and timestamp of insertion. If there is a collision, override the previous entry. The hash table is meant to act as a caching mechanism.
-    public void addClient (String clientID, String email) {
-        
+    public static void addClient (String clientID, String email) {
+
     }
-    
+
     // Calculates the hash value derived from the clientID. Since the hash table has a maximum of 10 entries, the hash function will return integers 0 to 9
-    public int hashFunction (String clientID) {
+    public static int hashFunction (String clientID) {
         return 0;
     }
 }
